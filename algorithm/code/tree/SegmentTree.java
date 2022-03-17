e170cff6e616a0cf28f99103eff51ee58c1ff7bf
H-f-society <1915140@gmail.com>
2022-03-17 14:17:27 +0800

    update

---
+++ algorithm/code/tree/SegmentTree.java
@@ -0,0 +1,20 @@
+/* 
+* @Author: root
+* @Date:   2022-03-17 10:36:25
+* @Last Modified by:   root
+* @Last Modified time: 2022-03-17 11:24:40
+* 线段树
+*/
+
+public class SegmentTree {
+
+    private static int[] nums = {1, 2, 3, 4, 5, 6, 7};
+
+    private static int[] segTreeArr = new int[nums.length << 2];
+
+    public static void main(String[] args) {
+
+    }
+
+
+}
