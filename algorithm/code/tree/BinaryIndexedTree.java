dd07ba213267c887610386a37b3619e6425f4dfc
H-f-society <1915140@gmail.com>
2022-03-17 09:40:25 +0800

    update

---
+++ algorithm/code/tree/BinaryIndexedTree.java
@@ -0,0 +1,96 @@
+/* 
+* @Author: root
+* @Date:   2022-03-17 08:40:52
+* @Last Modified by:   root
+* @Last Modified time: 2022-03-17 09:38:17
+* 
+* 
+*     树状数组 /  二叉索引数
+*---------------------------------------
+*
+*                          36
+*            10
+*      3            11
+*   1     3     5       7
+* --------------------------------------
+*/
+
+import java.util.Arrays;
+
+public class BinaryIndexedTree {
+
+    private static int[] nums = {1, 2, 3, 4, 5, 6, 7, 8};
+
+    public static void main(String[] args) {
+        BinaryIndexedTree bit = new BinaryIndexedTree();
+
+        bit.createBIT(nums);
+
+        System.out.println(bit.ask(nums, 5));
+        System.out.println(bit.ask(nums, 3, 6));
+
+        bit.print(nums);
+
+        bit.addVal(nums, 2, 7);
+
+        System.out.println(bit.ask(nums, 5));
+        System.out.println(bit.ask(nums, 3, 6));
+
+        bit.print(nums);
+    }
+
+    /**
+     * 创建树状数组
+     * Tree[x] 的父节点为 Tree[x + lowbit(i)]
+     * 父节点值 = 父节点+ 所有子节点
+     */
+    public void createBIT(int[] nums) {
+        for (int i=0; i<nums.length - 1; i++) {
+            nums[i + lowbit(i + 1)] += nums[i];
+        }
+    }
+
+    public void addVal(int[] nums, int index, int val) {
+        if (index >= nums.length) {
+            return;
+        }
+        nums[index] += val;
+        int pNodeIndex = index + lowbit(index + 1);
+        addVal(nums, pNodeIndex, val);
+    }
+
+    /**
+     * 下标 0 ~ index 的和
+     * 时间复杂度: O(logn)
+     */
+    public int ask(int[] nums, int index) {
+        int result = 0;
+        index += 1;
+        while(index > 0) {
+            result += nums[index - 1];
+            int temp = lowbit(index);
+            index -= temp;
+        }
+        return result;
+    }
+
+    public int ask(int[] nums, int beginIndex, int endIndex) {
+        return ask(nums, endIndex) - ask(nums, beginIndex - 1);
+    }
+
+    /**
+     * lowbit(x)是x的二进制表达式中最低位的1所对应的值
+     * -x = x取反 + 1
+     */
+    public int lowbit(int num) {
+        return num & -num;
+    }
+
+    public void print(int[] nums) {
+        for (int i=0; i<nums.length; i++) {
+            System.out.print( i == 0 ? "[ " +nums[i] : ", " + nums[i] + (i == nums.length - 1 ? " ]" : "") );
+        }
+        System.out.println();
+    }
+
+}
